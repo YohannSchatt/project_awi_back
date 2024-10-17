18d2a7bcfefee42832f967c012d02a7363048692
yohann.schatt <yohann.schatt@etu.umontpellier.fr>
2024-10-17 10:51:46 +0200

    ajout variable d'environnement et configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './authentification/auth/auth.module';
+import { UsersModule } from './authentification/users/users.module';
+import { Constants } from './constants';
+import configuration from './configuration';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true, // Rendre les variables accessibles globalement
+    envFilePath: '.env', // Spécifie le chemin du fichier .env
+    load: [configuration], // Charge la configuration
+  }),
+    AuthModule, 
+    UsersModule
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, Constants],
 })
 export class AppModule {}
---
+++ src/authentification/auth/auth.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { UsersModule } from '../users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './auth.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    UsersModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('jwt.secret'),
+        signOptions: { expiresIn: configService.get<string>('jwt.expiresIn') },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  providers: [AuthService],
+  controllers: [AuthController],
+  exports: [AuthService],
+})
+export class AuthModule {}
