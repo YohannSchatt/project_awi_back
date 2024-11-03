1c969761da4bf39e6b93f41f1064d06e0bafe7f5
Shane Donnelly <shane@donnelly>
2024-11-03 11:26:11 +0100

    ajouts mineurs sur les urls cors

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
  );
 
  app.enableCors({
-  origin: 'http://localhost:4200',
+  origin: ['http://localhost:4200', 'http://127.0.0.1:4200'],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization']
