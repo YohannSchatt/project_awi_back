67496ab1e691c0ae5b8c86b36052404c7c911186
shane donnelly <shane@donnelly>
2024-11-12 17:38:26 +0100

    ajouts de décorators sur vendeur et jeu

--- src/jeu/jeu.controller.ts
+++ src/jeu/jeu.controller.ts
@@ -1,12 +1,19 @@
 import { PositiveIntPipe } from './../pipe/positiveIntPipe';
-import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe, UseGuards } from '@nestjs/common';
 import { JeuService } from './jeu.service';
 import { CreateJeuDto } from './dto/create-jeu.dto';
 import { CatalogueDto } from './dto/response-catalogue.dto';
 import { UpdateJeuDto } from './dto/update-jeu.dto';
 import { CreateJeuUnitaireDto } from './dto/create-jeu-unitaire.dto';
 import { Jeu } from '@prisma/client';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/common/guards/roles.guard';
+import { Role } from '@prisma/client';
+
 @Controller('jeu')
+// @Roles([Role.ADMIN,Role.GESTIONNAIRE])
+// @UseGuards(JwtAuthGuard, RolesGuard)
 export class JeuController {
   constructor(private readonly jeuService: JeuService) {}
 
@@ -25,12 +32,15 @@ export class JeuController {
   createJeu(@Body() createJeuDto: CreateJeuDto) {
     return this.jeuService.createJeu(createJeuDto);
   }
-
+  @Roles([Role.ADMIN,Role.GESTIONNAIRE])
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Post('creerJeuUnitaire')
   createJeuUnitaire(@Body() createJeuUnitaireDto: CreateJeuUnitaireDto) {
     return this.jeuService.createJeuUnitaire(createJeuUnitaireDto);
   }
 
+  @Roles([Role.ADMIN,Role.GESTIONNAIRE])
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Get('listeJeu')
   getListeJeu(){
     return this.jeuService.getListeJeu();
--- src/vendeur/vendeur.controller.ts
+++ src/vendeur/vendeur.controller.ts
@@ -3,7 +3,14 @@ import { PositiveIntPipe } from 'src/pipe/positiveIntPipe';
 import { VendeurService } from './vendeur.service';
 import { CreateVendeurDto } from './dto/create-vendeur.dto';
 import { Vendeur } from '@prisma/client';
+import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/common/guards/roles.guard';
+import { Role } from '@prisma/client';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { UseGuards } from '@nestjs/common';
 
+@Roles([Role.ADMIN,Role.GESTIONNAIRE])
+@UseGuards(JwtAuthGuard, RolesGuard)
 @Controller('vendeur')
 export class VendeurController {
   constructor(private readonly vendeurService: VendeurService) {}
