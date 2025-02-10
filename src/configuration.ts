a2d216e2d820b4465d56b35e4d69db3a02d3f3d0
yohann.schatt <yohann.schatt@etu.umontpellier.fr>
2025-02-10 15:54:18 +0100

    modif confi.ts

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,5 +1,9 @@
 // This file is used to define the configuration of the application.
 
+const replaceUnderscoresWithSpaces = (str: string) => {
+  return str.replace(/_/g, ' ');
+};
+
 export default () => ({
     jwt: {
       secret: process.env.JWT_KEY || 'default_secret_key',
@@ -12,7 +16,7 @@ export default () => ({
       password: process.env.DATABASE_PASSWORD || 'default_password',
       url : process.env.DATABASE_URL || 'default_url',
       url_front : process.env.FRONTEND_URL || 'default_url_front',
-      email_mdp : process.env.EMAIL_MDP || 'default_email_mdp',
+      email_mdp : replaceUnderscoresWithSpaces(process.env.EMAIL_MDP) || 'default_email_mdp',
     },
     node_env: process.env.NODE_ENV || 'development',
   });
